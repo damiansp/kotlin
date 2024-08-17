75481022e28f052f85284d209279733fd1615754
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-08-17 11:26:07 -0700

    AMS++

--- udacity/bootcamp/2_Basics/basics/src/AMS.kt
+++ udacity/bootcamp/2_Basics/basics/src/AMS.kt
@@ -1,21 +1,50 @@
-import java.util.Calendar
+import java.util.Random
+import kotlin.math.tan
+
 
 fun main(args: Array<String>) {
-    getDayOfWeek()
+    val day = randDay()
+    feedFish(day)
+    if (waterNeedsChanging(day)) {
+        println("Change the water today!")
+    }
 }
 
 
-fun getDayOfWeek() {
-    print("Today is ")
-    val day = Calendar.getInstance().get(Calendar.DAY_OF_WEEK)
-    println(when (day) {
-        1 -> "Sun"
-        2 -> "Mon"
-        3 -> "Tue"
-        4 -> "Wed"
-        5 -> "Thu"
-        6 -> "Fri"
-        7 -> "Sat"
-        else -> "Unday"
-    })
+fun randDay(): String {
+    val week = listOf("Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun")
+    return week[Random().nextInt(7)]
+}
+
+
+fun feedFish(day: String) {
+    val food = getFood(day)
+    println("Today is $day, and the fish eat $food")
+}
+
+
+fun getFood(day: String): String {
+    return when (day) {
+        "Mon" -> "flakes"
+        "Tue" -> "pellets"
+        "Wed" -> "redworms"
+        "Fri" -> "mosquitoes"
+        "Sat" -> "seaweed"
+        "Sun" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+
+fun waterNeedsChanging(day: String, temp: Int=27, dirty: Int=20): Boolean {
+    return true
+}
+
+
+fun canAddFish(
+        tankSize: Int, currentFishLengths: Array<Int>, hasDecorations: Boolean
+): Boolean {
+    val capacity = tankSize * 0.8 if hasDecorations else tankSize
+    val currentLengths = currentFishLengths.sum()
+    return currentLengths < capacity
 }
