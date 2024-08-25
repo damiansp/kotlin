162d908d09e8a67e4b6533dc76027ff9ee6d63a4
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-08-24 22:20:03 -0700

    aq++

--- udacity/bootcamp/3_Beyond/beyond/src/Aquarium/Aquarium.kt
+++ udacity/bootcamp/3_Beyond/beyond/src/Aquarium/Aquarium.kt
@@ -24,4 +24,16 @@ internal fun buildAquarium() = Aquarium()
 open class Aquarium(var length: Int = 100, var width: Int = 20, var height: Int = 40) {
     open var volume: Int
         get() = width * height * length / 1000
-        set(value) { height = value * 1000 / (width * length) }
+        set(value) {
+            height = value * 1000 / (width * length)
+        }
+}
+
+
+fun String.hasSpaces() = find { it == ' ' } != null
+
+
+fun extensionsExample() {
+    "Does it have spaces?".hasSpaces()
+}
+
