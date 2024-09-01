7f8d5ac2925eda5661bae93512270dcd1830d0d3
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-08-31 21:37:43 -0700

    init birthday card app

--- android/birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ android/birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,29 +21,25 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HappyBirthdayTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(
+                    modifier=Modifier.fillMaxSize(), color=MaterialTheme.colorScheme.background) {
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun GreetingText(msg: String, modifier: Modifier = Modifier) {
+    Text(text=msg)
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground=true)
 @Composable
-fun GreetingPreview() {
+fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        Greeting("Android")
+        GreetingText(msg = "Happy Birthday, Fearthainn!")
     }
 }
