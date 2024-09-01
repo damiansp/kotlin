c341c3fab92a27f27894bc0f5756fcd24a58f9fa
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-08-31 23:41:24 -0700

    add more text and format into col

--- android/birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ android/birthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
@@ -13,7 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,8 +26,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Surface(
-                    modifier=Modifier.fillMaxSize(), color=MaterialTheme.colorScheme.background) {
-                }
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
+                ) {}
             }
         }
     }
@@ -31,8 +35,11 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun GreetingText(msg: String, modifier: Modifier = Modifier) {
-    Text(text=msg)
+fun GreetingText(msg: String, from: String, modifier: Modifier = Modifier) {
+    Column(modifier = modifier) {
+        Text(text = msg, fontSize = 76.sp, lineHeight = 80.sp)
+        Text(text = from, fontSize = 36.sp)
+    }
 }
 
 
@@ -40,6 +47,6 @@ fun GreetingText(msg: String, modifier: Modifier = Modifier) {
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(msg = "Happy Birthday, Fearthainn!")
+        GreetingText(msg = "Happy Birthday, Fearthainn!", from = "love Daddy")
     }
 }
